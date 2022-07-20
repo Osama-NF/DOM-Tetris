829d539413d712c294f6e097a6d47f65d8a41ed2
Osama-NF <orz14322@gmail.com>
2022-07-20 13:13:50 +0300

    finished dropBlock() function

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,8 @@ class Game {
     constructor(classic) {
 
         this.classic = classic;
+        this.stepDistance = $('main').height() / 10;
+        this.fieldLimits = $('main').height() - this.stepDistance * 2;
 
         this.blockTypes = [
             {
@@ -19,13 +21,13 @@ class Game {
                 blockName: "big-block",
                 divsNumber: 4
             },
-            {
-                blockName: "single-block",
-                divsNumber: 1
-            },
+            // {
+            //     blockName: "single-block",
+            //     divsNumber: 1
+            // },
         ];
 
-        // down here is just a ref for a feture idea on how to use the rotate()
+        // down here is just a ref for a feture idea on how to use the rotateBlock()
         this.blockDirection = {
             default: 1,
             right: 2,
@@ -36,28 +38,13 @@ class Game {
     }
 
     start() {
-        let container = this.createBlock()
-        $('main').append(container)
+        let container = this.createBlock();
+        $('main').append(container);
 
-        let fiedlLimits = $('main').height() - $('main').height() / 10;
-        let stepDistance = $('main').height() / 10;
-
-        // should change to a recursive function ???
-        let blockFall = setInterval(() => {
-            
-            let currentTopValue = Number($('.moveable').css('top').replace('px',''));
-            let newTopValue = currentTopValue + stepDistance;
-
-            $('.moveable').css('top', newTopValue);
-            
-            if (newTopValue >= fiedlLimits) {
-                clearInterval(blockFall);
-            }
-
-        }, 200)
-
+        this.dropBlock(container);
     }
 
+    // Below function creates the container for the block and returns it
     createBlock() {
         // Choose the type of the block randomly
         let randomNumber = Math.floor((Math.random() * this.blockTypes.length));
@@ -80,4 +67,24 @@ class Game {
         return container;
     }
 
+    // below is a recusrive function that continues to call it self as long as the block didn't hit the bottom (should be changed for the .moveable class later on)
+    dropBlock(container) {
+
+        let currentTopValue = Number($(container).css('top').replace('px',''));
+        if(currentTopValue >= this.fieldLimits){
+            console.log('current value: ' + currentTopValue)
+            console.log('field limit: ' + this.fieldLimits)
+            return;
+        }
+
+        let newValue = currentTopValue + this.stepDistance;
+        $(container).css('top', newValue);
+
+        setTimeout(() => {
+            console.log(currentTopValue)
+            this.dropBlock(container);
+        }, 100);
+        
+    }
+    
 }
