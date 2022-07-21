9655889301ce4a237cec92dfc80980cb517e0988
Osama-NF <osama.nhf@gmail.com>
2022-07-21 23:59:20 +0300

    half finished with moveBlock() function

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,8 @@ class Game {
     constructor(classic, dropSpeed) {
 
         this.classic = classic;
-        this.stepDistance = $('main').height() / dropSpeed;
+        this.stepDistanceY = $('main').height() / dropSpeed;
+        this.stepDistanceX = $('main').width() / dropSpeed;
         this.stepCounter = dropSpeed - (dropSpeed * 0.1);
 
         this.blockTypes = [
@@ -42,6 +43,15 @@ class Game {
         $('main').append(container);
 
         this.dropBlock(container, this.stepCounter);
+
+        document.addEventListener('keydown', (e) => {
+            switch(e.key) {
+                case "ArrowRight":
+                case "ArrowLeft":
+                    this.moveBlock(e.key);
+                    break;
+            }
+        });
     }
 
     // Below function creates the container for the block and returns it
@@ -67,16 +77,16 @@ class Game {
         return container;
     }
 
-    // below is a recusrive function that continues to call it self as long as the block didn't hit the bottom (should be changed for the .moveable class later on)
+    // below is a recusrive function that continues to call it self as long as the block 
+    // didn't hit the bottom (should be changed for the .moveable class later on)
     dropBlock(container, counter) {
 
         if ($(container).hasClass('moveable') && counter > 0) {
 
             let currentTopValue = Number($(container).css('top').replace('px',''));
-            let newValue = currentTopValue + this.stepDistance;
+            let newValue = currentTopValue + this.stepDistanceY;
             $(container).css('top', newValue);
             
-            console.log(counter)
             let newCounter = counter - 1;
             setTimeout(()=> {
                 this.dropBlock(container, newCounter);
@@ -89,4 +99,22 @@ class Game {
         
     }
     
+    moveBlock(dir) {
+        
+        let currentRightValue = Number($('.moveable').css('right').replace('px',''));
+        let leftBorders = $('main').width() / 10 * 9; // will change math later to make it shorter (also to move the code somewhere nicer)
+
+        if (dir == 'ArrowRight' && currentRightValue > 0) {
+            
+            let newRightValue = currentRightValue - this.stepDistanceX;
+            $('.moveable').css('right', newRightValue);
+
+        } else if (dir == 'ArrowLeft' && currentRightValue < leftBorders) {
+
+            let newRightValue = currentRightValue + this.stepDistanceX;
+            $('.moveable').css('right', newRightValue);
+            console.log(newRightValue)
+        }
+
+    }
 }
