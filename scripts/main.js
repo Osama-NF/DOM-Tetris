86dcad83f299c7c70393a6755cf400923d0bd608
Osama-NF <osama.nhf@gmail.com>
2022-07-24 23:55:13 +0300

    making the coordinations functions

--- scripts/main.js
+++ scripts/main.js
@@ -12,10 +12,14 @@ class Game {
         this.dropSpeedCopy = dropSpeed;
         this.dropDistance = dropDistance;
 
+        // this.rightWall = $('aside.right').position().left;
+        // this.leftWall = $('main').position().left;
+        // this.bottomFloor = $('main').height();
+
         this.stepDistanceY = $('main').height() / this.dropDistance;
         this.stepDistanceX = $('main').width() / this.dropDistance;
-        this.fieldBorderY = $('main').height() * 0.85;
-        this.fieldBorderX = $('main').width() * 0.9;
+        // this.fieldBorderY = $('main').height() * 0.85;
+        // this.fieldBorderX = $('main').width() * 0.9;
 
         this.blockTypes = [
             {
@@ -60,7 +64,7 @@ class Game {
             switch(pressedKey) {
                 
                 case "arrowright":
-                case "arrowright":
+                case "arrowleft":
                 case "d":
                 case "a":
                     this.moveBlock(pressedKey);
@@ -127,12 +131,13 @@ class Game {
     dropBlock(container) {
 
         let currentTopValue = Number($(container).css('top').replace('px',''));
+        let newValue = currentTopValue + this.stepDistanceY;
+        let thereIsObstacle = this.checkObstaclesInDirection(
+            this.getBlockCoordinates(container, newValue, 'bottom'));
 
-        if ($(container).hasClass('moveable') && currentTopValue <= this.fieldBorderY) {
+        if ($(container).hasClass('moveable') && !thereIsObstacle) {
 
-            let newValue = currentTopValue + this.stepDistanceY;
             $(container).css('top', newValue);
-            
             setTimeout(()=> {
                 this.dropBlock(container);
             }, this.dropSpeed)
@@ -214,7 +219,100 @@ class Game {
         
     }
 
-    checkObstacles(x,y) {
+    checkObstaclesInDirection(blocks, newValue, direction) {
+    
+        for (let block of blocks) {
 
+            if (direction === 'bottom') block.bottom + newValue
+            else if (direction === 'right') block.right + newValue
+            else if (direction === 'left') block.left + newValue
+            
+            let leftCheck = document.elementsFromPoint(block.left, block.bottom);
+            let rightCheck = document.elementsFromPoint(block.right, block.bottom);
+            
+            for (let check of [leftCheck, rightCheck]) {
+    
+                for (let element of check) {
+                    if (element.tagName === "ASIDE"
+                        || element.tagName === "FOOTER"
+                        || (element.classList.contains('block') 
+                            && !element.classList.contains('moveable'))) {
+
+                        return true;
+                    }
+                }
+            }
+        }
+    
+        return false;
+    }
+
+    getBlockCoordinates(container) {
+        let coordinates = [];
+    
+        for (let block of container.children) {
+            let rect = block.getBoundingClientRect();
+            let obj = {
+                right: rect.right,
+                left: rect.left,
+                bottom: rect.bottom
+            }
+            coordinates.push(obj)
+        }
+        console.log(coordinates)
+        return coordinates;
     }
 }
+
+// ###########################################################################
+// ###########################################################################
+// ###########################################################################
+// ###########################################################################
+// down Here is just for help and will delete later
+
+// function checkObstacles(blocks) {
+    
+//     for (let block of blocks) {
+        
+//         let leftCheck = document.elementsFromPoint(block.left, block.bottom);
+//         let rightCheck = document.elementsFromPoint(block.right, block.bottom);
+        
+//         for (let check of [leftCheck, rightCheck]) {
+
+//             for (element of check) {
+//                 if (element.tagName === "ASIDE"
+//                     || element.tagName === "FOOTER"
+//                     || element.tagName === "DIV") {
+
+//                     return true;
+//                 }
+//             }
+//         }
+//     }
+
+//     return false;
+// }
+
+// function getBlockCoordinates(container) {
+//     let coordinates = [];
+
+//     for (let block of container.children) {
+//         let rect = block.getBoundingClientRect();
+//         let obj = {
+//             right: rect.right,
+//             left: rect.left,
+//             bottom: rect.bottom
+//         }
+//         coordinates.push(obj)
+//     }
+
+//     return coordinates;
+// }
+
+document.addEventListener('click', e => {
+    let y = e.clientY;
+    let x = e.clientX;
+    console.log('Y: ' + y)
+    console.log('X: ' + x)
+    console.log('###############')
+})
